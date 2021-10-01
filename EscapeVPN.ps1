bf7c251cbd679042e61158d8121ecc2affcd13d2
Gary Welch <welch.gary@gmail.com>
2021-10-01 12:47:40 -0500

    Formatting Update

--- EscapeVPN.ps1
+++ EscapeVPN.ps1
@@ -5,7 +5,7 @@ Clear-Host
 # Create some useful Splats
 $PriorityRoute = @{
     RouteMetric = 1
-    NextHop = "0.0.0.0"
+    NextHop     = "0.0.0.0"
 }
 
 $DisableAutoMetric = @{
@@ -18,24 +18,25 @@ $EnableAutoMetric = @{
 
 $interfaces = Get-NetIPInterface -AddressFamily IPv4
 
-If ($interfaces.InterfaceAlias -contains "VPN"){    
+If ($interfaces.InterfaceAlias -contains "VPN") {    
     Write-Host "Found VPN Adapter... Press any key to skip auto application"
     Start-Sleep -Milliseconds 500
     $Host.UI.RawUI.FlushInputBuffer()
     $waitTime = 0
-    While ( (-not $Host.UI.RawUI.KeyAvailable) -and ($waitTime -le 5)){        
+    While ( (-not $Host.UI.RawUI.KeyAvailable) -and ($waitTime -le 5)) {        
         Start-Sleep -Seconds 1
         $waitTime++
     }
 }
 
-If (-not $Host.UI.RawUI.KeyAvailable){
+If (-not $Host.UI.RawUI.KeyAvailable) {
     $int = [array]::IndexOf($interfaces.InterfaceAlias, "VPN")
-} Else {
+}
+Else {
     Clear-Host
 
     Write-Host -ForegroundColor Green "Interfaces"
-    For ($int = 0; $int -lt $interfaces.count; $int++){
+    For ($int = 0; $int -lt $interfaces.count; $int++) {
         Write-Host -ForegroundColor Yellow "$int. " -NoNewline
         Write-Host $interfaces[$int].InterfaceAlias
     }
@@ -45,7 +46,7 @@ If (-not $Host.UI.RawUI.KeyAvailable){
 $intAlias = $interfaces[$int].InterfaceAlias
 Clear-Host
 
-If ($interfaces[$int].AutomaticMetric -eq "Enabled"){
+If ($interfaces[$int].AutomaticMetric -eq "Enabled") {
     Write-Output "Disabled auto interface on $intAlias"
     Set-NetIPInterface @DisableAutometric `
         -InterfaceAlias $intAlias `
@@ -53,7 +54,8 @@ If ($interfaces[$int].AutomaticMetric -eq "Enabled"){
     Set-NetIPInterface @DisableAutometric `
         -InterfaceAlias $intAlias `
         -AddressFamily IPv6
-} Else {
+}
+Else {
     Write-Output "Enabled auto interface on $intAlias"
     Set-NetIPInterface @EnableAutoMetric `
         -InterfaceAlias $intAlias `
@@ -65,17 +67,18 @@ If ($interfaces[$int].AutomaticMetric -eq "Enabled"){
 
 $routes = @("172.20.0.0/16", "10.5.0.0/16", "10.85.0.0/16")
 
-ForEach($route in $routes){
+ForEach ($route in $routes) {
     If (
         Get-NetRoute `
             -InterfaceAlias $intAlias `
             -DestinationPrefix $route
-    ){
+    ) {
         Write-Host "Updated route for $route"
         Set-NetRoute @PriorityRoute `
             -InterfaceAlias $intAlias `
             -DestinationPrefix $route
-    } Else {
+    }
+    Else {
         Write-Host "Created route for $route"
         New-NetRoute @PriorityRoute `
             -InterfaceAlias $intAlias `
@@ -83,9 +86,9 @@ ForEach($route in $routes){
     }
 }
 
-If ($intAlias -ne "VPN" -and ((Get-NetAdapter).Name -notcontains "VPN")){
+If ($intAlias -ne "VPN" -and ((Get-NetAdapter).Name -notcontains "VPN")) {
     $ren = Read-Host -Prompt "Rename interface to VPN?"
-    If ($ren -eq "y"){
+    If ($ren -eq "y") {
         Rename-NetAdapter -Name $intAlias -NewName "VPN"
     }
 }
--- README.md
+++ README.md
@@ -1,15 +1,35 @@
 # Escape VPN
+
 ## Who is it for
-Intended for use by anyone that is using Windows and has a captive VPN that they would like to work around.  
-*Note:* If your workplace uses a captive VPN, they likely have a good reason for it. Running this may result in a negative interaction with your employer - Use at your own risk
+
+Intended for use by anyone that is using Windows and has a captive VPN that they
+would like to work around.  
+*Note:* If your workplace uses a captive VPN, they likely have a good reason for
+it. Running this may result in a negative interaction with your employer - Use
+at your own risk
+
 ## What does it do
-It prevents traffic from going over the VPN connection, unless it is destined for a buesiness network. Update the $route variables as needed for your VPN
+
+It prevents traffic from going over the VPN connection, unless it is destined
+for a buesiness network. Update the $route variables as needed for your VPN
+
 ## Where may I?
-Don't know. Anywhere really. Work, home, vacation resort. Just be wise about it. If you're disabling this it will may allow other devices to interact with yours.
+
+Don't know. Anywhere really. Work, home, vacation resort. Just be wise about it.
+If you're disabling this it will may allow other devices to interact with yours.
+
 ## When could I?
-When running any captive VPN, after the connection has been established of course.
+
+When running any captive VPN, after the connection has been established of
+course.
+
 ## Why would I?
-Because you can. Because you want to be able to watch Youtube or play games while connected to your work computer to multi-task. Couldn't say for sure. There may be moral or ehtical issues with this.  
+
+Because you can. Because you want to be able to watch Youtube or play games
+while connected to your work computer to multi-task. Couldn't say for sure.
+There may be moral or ehtical issues with this.  
   
+
 ## Usage
+
 Download .ps1, open powershell as Admin, run with .\EscapeVPN.ps1
